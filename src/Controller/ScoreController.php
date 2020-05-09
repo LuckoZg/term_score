852c1d6dda5c016b57e49d8c293b6988881976ec
Lucian Tutunovic <lucian.tutunovic@styria.hr>
2020-05-09 19:20:36 +0200

    Add $provider parameter

--- src/Controller/ScoreController.php
+++ src/Controller/ScoreController.php
@@ -8,11 +8,11 @@ use Symfony\Component\Routing\Annotation\Route;
 class ScoreController
 {
     /**
-     * @Route("/score/{term}", name="score")
+     * @Route("/score/{term}/{provider}", name="score")
      */
-    public function getScore(string $term)
+    public function getScore(string $term, string $provider = "github")
     {
-        return new JsonResponse(['term' => $term]);
+        return new JsonResponse(['term' => $term, 'provider' => $provider]);
 
         // the shortcut defines three optional arguments
         // return $this->json($data, $status = 200, $headers = [], $context = []);
