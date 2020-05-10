303be9dbe7737c3f2265865123c94d0f7362aaed
Lucian Tutunovic <lucian.tutunovic@styria.hr>
2020-05-10 12:46:55 +0200

    Refactor controller  - implement get_results_from_provider method

--- src/Controller/ScoreController.php
+++ src/Controller/ScoreController.php
@@ -14,13 +14,11 @@ class ScoreController
 
     private $providers_namespace = 'App\Provider\\';
     private $providers = array(
-        'github' => 'GitHubProvider',
-        'twitter' => 'TwitterProvider'
+        'github' => 'GitHubProvider'
     );
 
     private $error_messages = array(
-        'provider_not_available' => 'Provider is not available.',
-        'provider_not_implemented' => 'Provider is not implemented.'
+        'provider_not_available' => 'Provider is not available.'
     );
 
     /**
@@ -36,25 +34,25 @@ class ScoreController
         // Check if term score for provider is already saved into database
 
         // Fetch data from provider
-            $client = HttpClient::create();
+        $results = $this->get_results_from_provider($term, $provider);
+        $score = $this->get_full_score($results);
 
-            $provider_class = $this->providers_namespace.$this->providers[$provider];
-            $provider = new $provider_class;
-
-            try {
-                $results = $provider->get_results($client, $term, $this->term_positive, $this->term_negative);
-            } catch (\Throwable $th) {
-                return new JsonResponse(['status_message' => $this->error_messages['provider_not_implemented']], $status=500);
-            }
-
-            $score = $this->get_full_score($results);
-
-            // Post/Update data to database in another thread (async)
+        // Post/Update data to database in another thread (async)
 
         return new JsonResponse(['term' => $term, 'score' => $score], $status=200);
     }
 
-    private function get_full_score($results){
+    private function get_results_from_provider($term, $provider)
+    {
+        $client = HttpClient::create();
+        $provider_class = $this->providers_namespace.$this->providers[$provider];
+        $provider = new $provider_class;
+        
+        return $provider->get_results($client, $term, $this->term_positive, $this->term_negative);
+    }
+
+    private function get_full_score($results)
+    {
         $sum_count = $results['positive_count'] + $results['negative_count'];
         $score = ($results['positive_count'] / $sum_count) * $this->factor;
 
