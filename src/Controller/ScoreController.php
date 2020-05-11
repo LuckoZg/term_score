67b2fcef53f0aa957a5672289cfe4a515e41591f
Lucian Tutunovic <lucian.tutunovic@styria.hr>
2020-05-11 22:11:45 +0200

    Added comments

--- src/Controller/ScoreController.php
+++ src/Controller/ScoreController.php
@@ -18,10 +18,21 @@ class ScoreController extends AbstractController
     private $db_score_expires = "+7 day";
 
     /**
+     * Get popularity score of given term on external services (providers)
+     * like GitHub or Twitter.
+     * 
+     * Example GET calls:
+     *  - /api/v1/php/
+     *  - /api/v1/php/twitter
+     * 
+     * GitHub is default provider, so you don't need to explicitly call:
+     *  - /api/v1/php/github (but certainly you can do that)
+     * 
      * @Route("/score/{term}/{provider}", name="score", methods={"GET"})
      */
     public function get_score(string $term, string $provider = 'github'): JsonResponse
     {
+        // Validate if provider exists
         if(!$this->validate_provider($provider)){
             return new JsonResponse(
                 $data=['status_message' => $this->getParameter('app.error_messages')['provider_not_available']], 
@@ -29,6 +40,7 @@ class ScoreController extends AbstractController
             );
         }
 
+        // Get score from database or from external API
         $term = trim($term);
         $score = $this->get_score_from_database($term, $provider);
         if(!$score){
@@ -36,6 +48,7 @@ class ScoreController extends AbstractController
             $this->set_score_to_database($term, $provider, $score);
         }
 
+        // Return score to client
         return new JsonResponse(
             $data=['term' => $term, 'score' => $score], 
             $status=JsonResponse::HTTP_OK
@@ -44,7 +57,11 @@ class ScoreController extends AbstractController
 
     private function validate_provider($provider): bool
     {
-        if(!isset(ProviderFactory::providers[$provider]) || !class_exists(ProviderFactory::get_provider_class($provider))){
+        /**
+         * Validate if provider is defined in ProviderFactory class as a constant
+         * and if class exists in providers namespace.
+         */
+        if(!isset(ProviderFactory::providers[$provider]) || !class_exists(ProviderFactory::get_provider_classname($provider))){
             return false;
         }
 
@@ -53,6 +70,10 @@ class ScoreController extends AbstractController
 
     private function get_full_score($results): float
     {
+        /**
+         * Algorithm which returns popularity on scale 1-10 based on arrays 
+         * of positive and negative sum.
+         */
         $sum_count = $results['positive_count'] + $results['negative_count'];
         if($sum_count == 0){
             return round(($sum_count), 2);
@@ -65,6 +86,9 @@ class ScoreController extends AbstractController
 
     private function get_score_from_provider($term, $provider): float
     {
+        /**
+         * Get popularity score for given term from external service (like GitHub).
+         */
         $client = HttpClient::create();
         $provider = ProviderFactory::get_provider_instance($provider);
         $results = $provider->get_results($client, $term, $this->term_positive, $this->term_negative);
@@ -74,6 +98,10 @@ class ScoreController extends AbstractController
 
     private function get_score_from_database($term, $provider): ?float
     {
+        /**
+         * Get popularity score for given term and provider from database.
+         * We use this method so we can query faster results with database layer using as cache.
+         */
         $entityManager = $this->getDoctrine()->getManager();
         $query = $entityManager->createQuery('
             SELECT term
@@ -93,6 +121,9 @@ class ScoreController extends AbstractController
 
     private function set_score_to_database($term, $provider, $score): void
     {
+        /**
+         * Post or update score for given term and provider in database. 
+         */
         $entityManager = $this->getDoctrine()->getManager();
         $query = $entityManager->createQuery('
             SELECT term
@@ -114,6 +145,9 @@ class ScoreController extends AbstractController
 
     private function update_term($entityManager, $term_from_db, $score): void
     {
+        /**
+         * Update existing term with new score and expires (when cache is invalid) date.
+         */
         $term_from_db->setScore($score);
         $term_from_db->setExpires(new \DateTime(gmdate('Y-m-d H:i:s', strtotime($this->db_score_expires))));
         $entityManager->flush();
@@ -121,6 +155,9 @@ class ScoreController extends AbstractController
 
     private function create_term($entityManager, $term, $provider, $score): void
     {
+        /**
+         * Create new term with all parameters.
+         */
         $new_term = new Term();
         $new_term->setName($term);
         $new_term->setProvider($provider);
--- src/Factory/ProviderFactory.php
+++ src/Factory/ProviderFactory.php
@@ -13,12 +13,12 @@ class ProviderFactory
 
     public static function get_provider_instance($provider): Provider
     {
-        $provider_class = self::providers_namespace.self::providers[$provider];
+        $provider_class = self::get_provider_classname($provider);
         
         return new $provider_class;
     }
 
-    public static function get_provider_class($provider): string
+    public static function get_provider_classname($provider): string
     {
         return self::providers_namespace.self::providers[$provider];
     }
